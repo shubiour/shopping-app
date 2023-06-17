06e409513c2c56d8a6b79bdbdf0603f51ad7f4fa
Shubiour Shuvo <sv95.shuvo@gmail.com>
2023-06-17 16:43:12 +0600

    counter badge added over cart icon

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,23 +1,44 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopping_app/Screen/cart_screen.dart';
 import 'package:shopping_app/Screen/product_detail_screen.dart';
+import '../Controller/cart_item_controller.dart';
 import '../Controller/home_controller.dart';
 
 class HomeScreen extends StatelessWidget {
   final HomeController controller = Get.find<HomeController>();
+  final CartController cartController = Get.find<CartController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+       appBar: AppBar(
         title: Text('Product App'),
         actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              Get.to(CartScreen());
-            },
+          Stack(
+            children: [
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {
+                  Get.to(CartScreen());
+                },
+              ),
+              Positioned(
+                right: 0,
+                child: Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Obx(() => Text(
+                    '${cartController.cartItemCount}',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ),
+              ),
+            ],
           ),
         ],
       ),
@@ -26,7 +47,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
+              padding: const EdgeInsets.only(bottom: 10.0),
               child: TextField(
                 onChanged: controller.searchProducts,
                 decoration: InputDecoration(
@@ -56,13 +77,11 @@ class HomeScreen extends StatelessWidget {
                             Get.to(ProductDetailsScreen(product: product));
                           },
                           child: GridTile(
-                            child:
-                                Image.network(product.image, fit: BoxFit.cover),
+                            child: Image.network(product.image, fit: BoxFit.cover),
                             footer: GridTileBar(
                               backgroundColor: Colors.black45,
                               title: Text(product.title),
-                              subtitle:
-                                  Text('\$${product.price.toStringAsFixed(2)}'),
+                              subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
                             ),
                           ),
                         );
@@ -78,3 +97,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
