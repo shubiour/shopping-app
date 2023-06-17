b3e91da122974a37598b7a50e4f1d1cd1c0a92be
Shubiour Shuvo <sv95.shuvo@gmail.com>
2023-06-17 16:28:18 +0600

    grid view in home page

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shopping_app/Screen/cart_screen.dart';
 import 'package:shopping_app/Screen/product_detail_screen.dart';
 import '../Controller/home_controller.dart';
+
 class HomeScreen extends StatelessWidget {
   final HomeController controller = Get.find<HomeController>();
 
@@ -10,48 +12,69 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Product App'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: controller.searchProducts,
-              decoration: InputDecoration(
-                labelText: 'Search',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Obx(
-              () {
-                if (controller.products.isEmpty) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  return ListView.builder(
-                    itemCount: controller.filteredProducts.length,
-                    itemBuilder: (context, index) {
-                      final product = controller.filteredProducts[index];
-                      return ListTile(
-                        title: Text(product.title),
-                        subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
-                        leading: Image.network(product.image),
-                        onTap: () {
-                          Get.to(ProductDetailsScreen(product: product));
-                        },
-                      );
-                    },
-                  );
-                }
-              },
-            ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Get.to(CartScreen());
+            },
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                onChanged: controller.searchProducts,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Obx(
+                () {
+                  if (controller.filteredProducts.isEmpty) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 10.0,
+                        crossAxisSpacing: 10.0,
+                      ),
+                      itemCount: controller.filteredProducts.length,
+                      itemBuilder: (context, index) {
+                        final product = controller.filteredProducts[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Get.to(ProductDetailsScreen(product: product));
+                          },
+                          child: GridTile(
+                            child:
+                                Image.network(product.image, fit: BoxFit.cover),
+                            footer: GridTileBar(
+                              backgroundColor: Colors.black45,
+                              title: Text(product.title),
+                              subtitle:
+                                  Text('\$${product.price.toStringAsFixed(2)}'),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
