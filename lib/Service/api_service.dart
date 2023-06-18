245dd6b4ebb7ed7ac204906eba9b78178785d53c
Shubiour Shuvo <sv95.shuvo@gmail.com>
2023-06-18 11:45:12 +0600

    global url

--- lib/Service/api_service.dart
+++ lib/Service/api_service.dart
@@ -3,16 +3,19 @@ import 'package:http/http.dart' as http;
 import '../Model/product.dart';
 
 class ApiService {
-  static Future<List<Product>> fetchProducts() async {
-    const String baseUrl = 'https://fakestoreapi.com';
+  static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<List<Product>> fetchProducts() async {
     final response = await http.get(Uri.parse('$baseUrl/products'));
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
-      final List<Product> products = data.cast<Map<String, dynamic>>().map<Product>((json) => Product.fromJson(json)).toList();
+      final List<Product> products = data
+          .cast<Map<String, dynamic>>()
+          .map<Product>((json) => Product.fromJson(json))
+          .toList();
       return products;
     } else {
       throw Exception('Failed to fetch products');
     }
   }
 }
-
